09d799cedd83ffb74bd56440778de21fe1bd0ead
ersajo <sanchezjoserick@gmail.com>
2018-07-31 18:35:14 -0500

    Falta tomar valores para realizar calculo de varianza

--- samples/emg-data-sample.cpp
+++ samples/emg-data-sample.cpp
@@ -1,114 +1,118 @@
 // Copyright (C) 2013-2014 Thalmic Labs Inc.
 // Distributed under the Myo SDK license agreement. See LICENSE.txt for details.
 
-// This sample illustrates how to use EMG data. EMG streaming is only supported for one Myo at a time.
+// This sample illustrates how to log EMG and IMU data. EMG streaming is only supported for one Myo at a time, and this entire sample is geared to one armband
 
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include <iomanip>
+#include <algorithm>
 #include <array>
 #include <iostream>
 #include <sstream>
 #include <stdexcept>
 #include <string>
+#include <fstream>
+#include <time.h>
 
 #include <myo/myo.hpp>
 
 class DataCollector : public myo::DeviceListener {
 public:
-    DataCollector()
-    : emgSamples()
-    {
-    }
+	int count = 0;
 
-    // onUnpair() is called whenever the Myo is disconnected from Myo Connect by the user.
-    void onUnpair(myo::Myo* myo, uint64_t timestamp)
-    {
-        // We've lost a Myo.
-        // Let's clean up some leftover state.
-        emgSamples.fill(0);
-    }
+	DataCollector()
+	{
+		openFiles();
+	}
 
-    // onEmgData() is called whenever a paired Myo has provided new EMG data, and EMG streaming is enabled.
-    void onEmgData(myo::Myo* myo, uint64_t timestamp, const int8_t* emg)
-    {
-        for (int i = 0; i < 8; i++) {
-            emgSamples[i] = emg[i];
-        }
-    }
+	void openFiles() {
+		time_t timestamp = std::time(0);
 
-    // There are other virtual functions in DeviceListener that we could override here, like onAccelerometerData().
-    // For this example, the functions overridden above are sufficient.
+		// Open file for EMG log
+		if (emgFile.is_open()) {
+			emgFile.close();
+		}
+		std::ostringstream emgFileString;
+		//emgFileString << "b-Sofi" << ".csv";
+		//emgFileString << "b-Alex" << ".csv";
+		emgFileString << "b-Erick" << ".csv";
+		emgFile.open(emgFileString.str(), std::ios::out);
+		emgFile << "emg1,emg2,emg3,emg4,emg5,emg6,emg7,emg8" << std::endl;
 
-    // We define this function to print the current values that were updated by the on...() functions above.
-    void print()
-    {
-        // Clear the current line
-        std::cout << '\r';
+	}
 
-        // Print out the EMG data.
-        for (size_t i = 0; i < emgSamples.size(); i++) {
-            std::ostringstream oss;
-            oss << static_cast<int>(emgSamples[i]);
-            std::string emgString = oss.str();
+	// onEmgData() is called whenever a paired Myo has provided new EMG data, and EMG streaming is enabled.
+	void onEmgData(myo::Myo* myo, uint64_t timestamp, const int8_t* emg)
+	{
+		if (this->count != 100) {
+			for (size_t i = 0; i < 8; i++) {
+				emgFile << static_cast<int>(emg[i]) << ',';
+			}
+			emgFile << std::endl;
+			this->count++;
+		}
+	}
 
-            std::cout << '[' << emgString << std::string(4 - emgString.size(), ' ') << ']';
-        }
+	void onConnect(myo::Myo *myo, uint64_t timestamp, myo::FirmwareVersion firmwareVersion) {
+		//Reneable streaming
+		myo->setStreamEmg(myo::Myo::streamEmgEnabled);
+		openFiles();
+	}
 
-        std::cout << std::flush;
-    }
+	// The files we are logging to
+	std::ofstream emgFile;
 
-    // The values of this array is set by onEmgData() above.
-    std::array<int8_t, 8> emgSamples;
 };
 
 int main(int argc, char** argv)
 {
-    // We catch any exceptions that might occur below -- see the catch statement for more details.
-    try {
+	// We catch any exceptions that might occur below -- see the catch statement for more details.
+	try {
 
-    // First, we create a Hub with our application identifier. Be sure not to use the com.example namespace when
-    // publishing your application. The Hub provides access to one or more Myos.
-    myo::Hub hub("com.example.emg-data-sample");
+		// First, we create a Hub with our application identifier. Be sure not to use the com.example namespace when
+		// publishing your application. The Hub provides access to one or more Myos.
+		myo::Hub hub("com.undercoveryeti.myo-data-capture");
 
-    std::cout << "Attempting to find a Myo..." << std::endl;
+		std::cout << "Attempting to find a Myo..." << std::endl;
 
-    // Next, we attempt to find a Myo to use. If a Myo is already paired in Myo Connect, this will return that Myo
-    // immediately.
-    // waitForMyo() takes a timeout value in milliseconds. In this case we will try to find a Myo for 10 seconds, and
-    // if that fails, the function will return a null pointer.
-    myo::Myo* myo = hub.waitForMyo(10000);
+		// Next, we attempt to find a Myo to use. If a Myo is already paired in Myo Connect, this will return that Myo
+		// immediately.
+		// waitForMyo() takes a timeout value in milliseconds. In this case we will try to find a Myo for 10 seconds, and
+		// if that fails, the function will return a null pointer.
+		myo::Myo* myo = hub.waitForMyo(10000);
 
-    // If waitForMyo() returned a null pointer, we failed to find a Myo, so exit with an error message.
-    if (!myo) {
-        throw std::runtime_error("Unable to find a Myo!");
-    }
+		// If waitForMyo() returned a null pointer, we failed to find a Myo, so exit with an error message.
+		if (!myo) {
+			throw std::runtime_error("Unable to find a Myo!");
+		}
 
-    // We've found a Myo.
-    std::cout << "Connected to a Myo armband!" << std::endl << std::endl;
+		// We've found a Myo.
+		std::cout << "Connected to a Myo armband! Logging to the file system. Check the folder this appliation lives in." << std::endl << std::endl;
 
-    // Next we enable EMG streaming on the found Myo.
-    myo->setStreamEmg(myo::Myo::streamEmgEnabled);
+		// Next we enable EMG streaming on the found Myo.
+		myo->setStreamEmg(myo::Myo::streamEmgEnabled);
 
-    // Next we construct an instance of our DeviceListener, so that we can register it with the Hub.
-    DataCollector collector;
+		// Next we construct an instance of our DeviceListener, so that we can register it with the Hub.
+		DataCollector collector;
 
-    // Hub::addListener() takes the address of any object whose class inherits from DeviceListener, and will cause
-    // Hub::run() to send events to all registered device listeners.
-    hub.addListener(&collector);
+		// Hub::addListener() takes the address of any object whose class inherits from DeviceListener, and will cause
+		// Hub::run() to send events to all registered device listeners.
+		hub.addListener(&collector);
 
-    // Finally we enter our main loop.
-    while (1) {
-        // In each iteration of our main loop, we run the Myo event loop for a set number of milliseconds.
-        // In this case, we wish to update our display 50 times a second, so we run for 1000/20 milliseconds.
-        hub.run(1000/20);
-        // After processing events, we call the print() member function we defined above to print out the values we've
-        // obtained from any events that have occurred.
-        collector.print();
-    }
+		// Finally we enter our main loop.
+		while (1) {
+			// In each iteration of our main loop, we run the Myo event loop for a set number of milliseconds.
+			// In this case, we wish to update our display 50 times a second, so we run for 1000/20 milliseconds.
+			hub.run(1);
+		}
 
-    // If a standard exception occurred, we print out its message and exit.
-    } catch (const std::exception& e) {
-        std::cerr << "Error: " << e.what() << std::endl;
-        std::cerr << "Press enter to continue.";
-        std::cin.ignore();
-        return 1;
-    }
+		// If a standard exception occurred, we print out its message and exit.
+	}
+	catch (const std::exception& e) {
+		std::cerr << "Error: " << e.what() << std::endl;
+		std::cerr << "Press enter to continue.";
+		std::cin.ignore();
+		return 1;
+	}
 }
